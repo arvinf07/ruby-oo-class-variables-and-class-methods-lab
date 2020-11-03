326f72a37ddbfba0882a343cd545f13cdafd9ca8
Arvin Fernandez <arvinf07@gmail.com>
2020-11-03 13:14:17 -0500

    commit tree f9e544721d98c199e1340c93d55bf29b8f0ce62c

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,56 @@
-
+require 'pry'
 
 class Song
 
   attr_accessor :name, :genre, :artist
+  @@count, @@artists, @@genres = 0, [], []
 
-  def initialize(name, genre, artist)
+  def initialize(name, artist, genre)
     @name = name
     @genre = genre
     @artist = artist
+
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
   end  
+
+  def self.count
+    @@count
+  end  
+
+  def self.artists
+    @@artists.uniq
+  end  
+
+  def self.genres
+    @@genres.uniq
+  end  
+
+  def self.genre_count
+    genre_count = {}
+    @@genres.uniq.each {|genre| genre_count[genre] = 0}
+    @@genres.each do |genre|
+      if genre_count.keys.include?(genre)
+        genre_count[genre] += 1
+      else
+        genre_count[genre] = 0  
+      end  
+    end  
+    genre_count
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.uniq.each {|artist| artist_count[artist] = 0}
+    @@artists.each do |artist|
+      if artist_count.keys.include?(artist)
+        artist_count[artist] += 1
+      else
+        artist_count[artist] = 0  
+      end  
+    end  
+    artist_count
+  end  
+
 end
